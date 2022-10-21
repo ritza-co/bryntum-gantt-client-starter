c86030f43245c18a7dc05b6376bd4e539127af49
Matthew Clarke <m_d_clarke@hotmail.com>
2022-10-21 17:31:10 +0200

    added completed gantt

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ import "@bryntum/gantt/gantt.stockholm.css";
 
 const gantt = new Gantt({
   appendTo: document.body,
+  project: {
+    taskStore: {
+      autoTree: true,
+      transformFlatData: true,
+    },
+    transport: {
+      load: {
+        url: "http://localhost:1337/data",
+      },
+      sync: {
+        url: "http://localhost:1337/api",
+      },
+    },
+    autoLoad: true,
+    autoSync: true,
+    validateResponse: true,
+  },
 
   columns: [{ type: "name", width: 250, text: "Tasks" }],
 });
